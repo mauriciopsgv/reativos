d928d38cdb9e29b4201f42d8c28239c0eef9b95d
mauriciopsgv <mauriciopsgv@gmail.com>
2014-09-05 03:13:57 -0300

    Update firstgame.c

--- firstgame.c
+++ firstgame.c
@@ -44,13 +44,13 @@ void creating_enemys (Square* enemy, Color* colors){
 }
 
 void update_enemys_x (Square* enemy){
-	enemy->x += (( enemy->c.speed * enemy->direction[0] * (now - old))/1000000000);
+	enemy->x += enemy->c.speed *((enemy->direction[0] * (now - old))/1000000000);
 	printf("now - old = %d\n", now-old);
 	return;	
 }
 
 void update_enemys_y (Square* enemy){
-	enemy->y += ((enemy->c.speed * enemy->direction[1] * (now - old))/1000000000);
+	enemy->y += enemy->c.speed *((enemy->direction[1] * (now - old))/1000000000);
 	printf("now - old = %d\n", now-old);
 	return;	
 }
@@ -59,28 +59,28 @@ void collision_with_walls (Square * enemy){
 	if(enemy->x + enemy->c.width >= SCREEN_X){
 		enemy->x = SCREEN_X - enemy->c.width;
 		enemy->direction[0] = (enemy->direction[0] * (-1));
-		printf("enemy->c.speed : %d\n", enemy->c.speed);
+		enemy->x += enemy->c.speed *((enemy->direction[0] * (now - old))/1000000000);
 		return;
 	}
 
 	if(enemy->x <= 0){
 		enemy-> x = 0;
 		enemy->direction[0] = (enemy->direction[0] * (-1));
-		printf("enemy->c.speed : %d\n", enemy->c.speed);
+		enemy->x += enemy->c.speed *((enemy->direction[0] * (now - old))/1000000000);
 		return; 
 	}
 
 	if(enemy-> y + enemy->c.length >= SCREEN_Y){
 		enemy-> y = SCREEN_Y - enemy->c.length;
 		enemy->direction[1] = (enemy->direction[1] * (-1));
-		printf("enemy->c.speed : %d\n", enemy->c.speed);
+		enemy->y += enemy->c.speed *((enemy->direction[1] * (now - old))/1000000000);
 		return;
 	}
 
 	if(enemy->y <= 0){
 		enemy-> y = 0;
 		enemy->direction[1] = (enemy->direction[1] * (-1));
-		printf("enemy->c.speed : %d\n", enemy->c.speed);
+		enemy->y += enemy->c.speed *((enemy->direction[1] * (now - old))/1000000000);
 		return;
 	}
 }
