0c613cb87c5156b1b287e5ddeb9fa0f157eb6626
mauriciopsgv <mauriciopsgv@gmail.com>
2014-09-04 23:36:01 -0300

    Update firstgame.c

--- firstgame.c
+++ firstgame.c
@@ -36,7 +36,7 @@ void creating_enemys (Square* enemy, Color* colors){
 	enemy->y = rand()% (481-colors[j].length);
 	enemy->direction[0] = (int) sin(theta)*100; 
 	enemy->direction[1] = (int) cos(theta)*100;
-	enemy->c = &colors[j];
+	enemy->c = colors[j];
 }
 
 void update_enemys_x (Square* enemy){
@@ -57,6 +57,7 @@ int main(int argc, char* args[]){
 	SDL_Window* window = SDL_CreateWindow("Squares", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640,480, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, 1, 0);
 	SDL_Event e;
+	SDL_rect r;
 	int i;
 
 	//Declaration of Colors
@@ -124,7 +125,8 @@ int main(int argc, char* args[]){
 
 		for(i=0; i<5; i++){
 			SDL_SetRenderDrawColor(renderer, enemys[i].c.R,enemys[i].c.G,enemys[i].c.B,0x00);
-			SDL_RenderFillRect(renderer, {enemys[i].x, enemys[i].y, enemys[i].width, enemys[i].length});
+			r = {enemys[i].x, enemys[i].y, enemys[i].width, enemys[i].length};
+			SDL_RenderFillRect(renderer, r);
 		}
 
 	}
