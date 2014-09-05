f9bec957aa493c2336243396a216138a1c4a0d87
mauriciopsgv <mauriciopsgv@gmail.com>
2014-09-04 22:46:29 -0300

    Update firstgame.c
    Enemie movement smarter and colision still untreated

--- firstgame.c
+++ firstgame.c
@@ -1,14 +1,19 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
-#define T_BLUE 50
-#define T_RED 20
-#define T_GREEN 35
-#define wait 250
+#include <time.h>
+#include <math.h>
+#define B_SIZE 50
+#define B_SPEED 1
+#define SPEED_BLUE
+#define R_SIZE 20
+#define R_SPEED 3
+#define G_SIZE 35
+#define G_SPEED 2
 
 unsigned int now;
 unsigned int old = 0;
 
-typedef struct colors{
+typedef struct color{
 	int width;
 	int length;
 	int speed;
@@ -20,17 +25,114 @@ typedef struct colors{
 typedef struct square{
 	int x;
 	int y;
+	int direction[2];
 	Color* c;
 } Square;
 
 void creating_enemies (Square* enemie, Color* color){
+	float angle = rand()%360;
+	int j = rand()%3;
 	enemie->x = rand()% (641-color->width);
 	enemie->y = rand()% (481-color->length);
+	enemie->direction = {(int*) sin(angle)*100, (int*) cos(angle)*100}; 
 	enemie->c = color;
 }
 
 void update_enemies_x (Square* enemie){
-	if(enemie->x + enemie->c->width + enemie->c->speed >= 640){
+	enemie->x += ( enemie->c->speed * enemie->direction[0] * (now - old))/100 ;
+	return;	
+}
+
+void update_enemies_y (Square* enemie){
+	enemie->y += (enemie->c->speed * enemie->direction[1] * (now - old))/100;
+	return;	
+}
+
+int main(int argc, char* args[]){
+
+	//INICIALIZATION
+	setSeed(time());
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Squares", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640,480, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, 1, 0);
+	SDL_Event e;
+	int i;
+
+	//Declaration of Colors
+	Color blue;
+	blue.width = B_SIZE;
+	blue.length = B_SIZE;
+	blue.speed = B_SPEED;
+	blue.R = 0x00;
+	blue.G = 0x00;
+	blue.B = 0xFF;
+
+	Color red;
+	red.width = R_SIZE;
+	red.length = R_SIZE;
+	red.speed = R_SPEED;
+	red.R = 0xFF;
+	red.G = 0x00;
+	red.B = 0x00;
+
+	Color green;
+	green.width = G_SIZE;
+	green.length = G_SIZE;
+	green.speed = G_SPEED;
+	red.R = 0x00;
+	red.G = 0xFF;
+	red.B = 0x00;
+
+	Color colors[3] ={ red, green, blue};
+
+
+	//Declaration of the hero
+	Square hero;
+	hero.x = 320; 
+	hero.y = 240; 
+	hero.c = &green; // as green is the all-around color, it's set as the default color
+
+	//Declaration of Enemies
+	Square enemies[5]  = {enemie1, enemie2, enemie3, enemie4, enemie5};
+	for(i=0; i<5; i++){
+		creating_enemies( &enemies[i], colors);
+	}
+	
+
+
+	//EXECUTION
+	while(1){
+
+		now = SDL_GetTicks();
+
+		if(SDL_PoolEvent(&e) == 1){
+			if(e.type == SDL_Quit)
+				break;
+		}
+
+		update_enemies_x(&enemie1);
+		update_enemies_y(&enemie1);
+		
+
+	//RENDERIZATION
+		SDL_SetRenderDrawColor(renderer, 0x00,0x00,0x00,0x00);
+		SDL_RenderFillRect(renderer, NULL);
+
+		for(i=0; i<5; i++){
+			SDL_SetRenderDrawColor(renderer, enemies[i]->c->R,enemies[i]->c->G,enemies[i]->c->B,0x00);
+			SDL_RenderFillRect(renderer, {enemies[i]->x, enemies[i]->y, enemies[i]->width, enemies[i]->length});
+		}
+
+	}
+
+	//FINALIZATION
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+
+}
+
+/*	if(enemie->x + enemie->c->width + enemie->c->speed >= 640){
 		enemie-> x = 640 - enemie->c->width;
 		enemie-> c->speed = enemie-> c->speed * (-1);
 		return;
@@ -42,11 +144,6 @@ void update_enemies_x (Square* enemie){
 		return; 
 	}
 
-	enemie->x += enemie->c->speed;
-	return;	
-}
-
-void update_enemies_y (Square* enemie){
 	if(enemie-> y + enemie->c->length + enemie->c->speed >= 480){
 		enemie-> y = 480 - enemie->c->length;
 		enemie-> c->speed = enemie-> c->speed * (-1);
@@ -59,79 +156,4 @@ void update_enemies_y (Square* enemie){
 		return;
 	}
 
-	enemie->y += enemie->c->speed;
-	return;	
-}
-
-int main(int argc, char* args[]){
-
-	//INICIALIZATION
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("Squares", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640,480, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, 1, 0);
-	SDL_Event e;
-
-	//Declaration of Colors
-	Color blue;
-	blue.width = T_BLUE;
-	blue.length = T_BLUE;
-	blue.speed = 10;
-	blue.R = 0x00;
-	blue.G = 0x00;
-	blue.B = 0xFF;
-
-	Color red;
-	red.width = T_RED;
-	red.length = T_RED;
-	red.speed = 30;
-	red.R = 0xFF;
-	red.G = 0x00;
-	red.B = 0x00;
-
-	Color green;
-	green.width = T_GREEN;
-	green.length = T_GREEN;
-	green.speed = 20;
-	red.R = 0x00;
-	red.G = 0xFF;
-	red.B = 0x00;
-
-	//Declaration of the hero
-	Square hero;
-	hero.x = 320; 
-	hero.y = 240; 
-	hero.c = &green; // as green is the all-around color, it's set as the default color
-
-	//Declaration of Enemies
-	Square enemie1;
-	creating_enemies(&enemie1, &green);
-
-
-	//EXECUTION
-	while(1){
-
-		SDL_PoolEvent(&e);
-		now = SDL_GetTicks();
-
-		//if(e != 0){}
-		
-		if(now > old + wait){
-			update_enemies_x(&enemie1);
-			update_enemies_y(&enemie1);
-		}
-
-		SDL_SetRenderDrawColor(renderer, 0x00,0x00,0x00,0x00);
-		SDL_RenderFillRect(renderer, NULL);
-
-		if(now > old + wait){
-			g_enemy = enemie1;
-			SDL_SetRenderDrawColor(renderer, g_enemy->c->R,g_enemy->c->G,g_enemy->c->B,0x00);
-			SDL_RenderFillRect(renderer, {g_enemy->x, g_enemy->y, g_enemy->width, g_enemy->length});
-		} 
-	} 
-
-	//FINALIZATION
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-}
+*/
