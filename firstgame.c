b328cecba8ab59ee5777da019726f163b5d47a1a
mauriciopsgv <mauriciopsgv@gmail.com>
2014-09-05 04:27:39 -0300

    Update firstgame.c

--- firstgame.c
+++ firstgame.c
@@ -92,13 +92,13 @@ void collision_with_walls (Square * enemy){
 
 void change_color (Square * hero, Color * colors){
 	cont_cores++;
-	hero->x = hero->x - hero->c.width/2;
-	hero->y = hero->y - hero->c.length/2;
+	hero->x = hero->x + hero->c.width/2;
+	hero->y = hero->y + hero->c.length/2;
 
 	hero->c = colors[cont_cores%3];
 
-	hero->x = hero->x + hero->c.width/2;
-	hero->y = hero->y + hero->c.length/2;
+	hero->x = hero->x - hero->c.width/2;
+	hero->y = hero->y - hero->c.length/2;
 }
 
 int main(int argc, char* args[]){
