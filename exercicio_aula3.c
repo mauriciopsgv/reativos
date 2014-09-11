082236a7b5c91f47bd520bfc646fbf2bf97cd1b3
mauriciopsgv <mauriciopsgv@gmail.com>
2014-09-10 23:27:16 -0300

    Task 04
    Square already suffers from gravity, change color and "flies"

--- exercicio_aula3.c
+++ exercicio_aula3.c
@@ -1,60 +1,176 @@
 #include <SDL2/SDL.h>
-#define DELAY 500
+#include <stdio.h>
+#define HERO_W 30
+#define HERO_L 30
+#define WALL_W 20
+#define WALL_L 480
+#define SCREEN_X 640
+#define SCREEN_Y 480
+#define GRAVITY_DT 60
+#define GRAVITY_ACC 8
+#define MIN_VEL 10
+#define MAX_VEL 57
 
-void MoveSquare(SDL_Rect* r, int command, int*vx, int*vy){
+int cont_cores=1;
+unsigned long now, dt, old;
 
-	r->x += vx[command];
-	r->y += vy[command];
+typedef struct color{
+	int R;
+	int G;
+	int B;
+}	Color;
+
+typedef struct square{
+	int x;
+	int y;
+	float vel;
+	Color* c;
+}	Square;
+
+//Gravity
+void gravity (Square* hero){
+
+	if(now - old >= GRAVITY_DT - hero->vel){
+		hero->y += 1;
+		old = SDL_GetTicks();
+		hero->vel += GRAVITY_ACC;
+		if(hero->vel > MAX_VEL)
+			hero->vel = MAX_VEL;
+	}
+}
+
+int move_square (Square* hero, int flying){
+	if(now - old >= GRAVITY_DT - hero->vel){
+		hero->y -= 1;
+		old = SDL_GetTicks();
+		hero->vel -= flying/40;
+		if(hero->vel < MIN_VEL)
+			hero->vel = MIN_VEL;
+
+		flying++;
+	}
+
+	return flying;
+}
+
+int check_ground (Square* hero){
+	if(hero->y + HERO_L >= SCREEN_Y)
+		return 1;
+	return 0;
 
 }
 
-int main(int argc, char* args[])
-{
-	// Initialization
-	int command, move;
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window =SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOW);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_Rect r1 ={100,50,50,50};
-	SDL_Rect r2 ={500,300,50,50};
-	SDL_Event e;
-	int vx = { 50, 0, -50, 0};
-	int vy = { 0, 50, 0, -50};
-	unsigned long old = SDL_GetTicks();
-
+void change_color (Square * hero, Color * colors){
+	cont_cores++;
+	hero->x = hero->x + HERO_W/2;
+	hero->y = hero->y + HERO_L/2;
 	
-	command =4;
-	old = SDL_GetTicks();
+	hero->c = colors[cont_cores%3];
 
-	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
+	hero->x = hero->x - HERO_W/2;
+	hero->y = hero->y - HERO_L/2;
+}
+
+
+int main (int argc, char* args[]){
+
+	//INICIALIZATION
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Squares", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_X, SCREEN_Y, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Event e;
+	SDL_Rect draw_h;
+	int get_out = 0, flying=50;
+
+	//Declaration of Colors
+	Color blue;
+	blue.R = 0x00;
+	blue.G = 0x00;
+	blue.B = 0xFF;
+
+	Color red;
+	red.R = 0xFF;
+	red.G = 0x00;
+	red.B = 0x00;
+
+	Color green;
+	green.R = 0x00;
+	green.G = 0xFF;
+	green.B = 0x00;
+
+	Color colors[3] ={red, green, blue};
+
+	//A hero is born
+	Square hero = {300, 220, 0, &green};
+	draw_h.w = HERO_W;
+	draw_h.h = HERO_L;
+
+	SDL_SetRenderDrawColor(renderer, 0x49,0x49,0x49,0x00);
 	SDL_RenderFillRect(renderer, NULL);
-	SDL_RenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
-	SDL_RenderFillRect(renderer, &r1);
+	SDL_SetRenderDrawColor(renderer, hero.c->R, hero.c->G, hero.c->B, 0x00);
+	draw_h.x =hero.x;
+	draw_h.y =hero.y;
+	SDL_RenderFillRect(renderer, &draw_h);
 	SDL_RenderPresent(renderer);
 
+	//EXECUTION
 
-
-	// Execution
 	while(1){
-		
 
-		if(SDL_GetTicks() > old + DELAY){
+		now = SDL_GetTicks();
+		dt = now - old;
 
-			MoveSquare(&r1,command%4, vx, vy);
-
-			SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
-			SDL_RenderFillRect(renderer, NULL);
-
-			SDL_RenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0x00);
-			SDL_RenderFillRect(renderer, &r1);
-
-			SDL_RenderPresent(renderer);
-			old = SDL_GetTicks();
-
-			move++;
-			if(move%2 == 0){
-				command++;
+			if(SDL_PollEvent(&e) == 1){
+				if(e.type == SDL_QUIT){
+					get_out = 1;
+					break;
+				}
+				else if( e.type == SDL_KEYDOWN){
+					switch (e.key.keysym.sym){
+					case SDLK_UP:
+						flying = 0;
+						break;
+					case SDLK_SPACE:
+						change_color(&hero, colors);
+						break;
+					}
+				}	
 			}
+
+
+		if(flying == 50){
+			gravity(&hero);
+			//get_out = check_ground(&hero);
+		}
+		else
+			flying = move_square(&hero, flying);
+
+		if(hero.y + HERO_L >= SCREEN_Y)
+			hero.y = SCREEN_Y - HERO_L;
+
+		//RENDERIZATION
+		SDL_SetRenderDrawColor(renderer, 0x49,0x49,0x49,0x00);
+		SDL_RenderFillRect(renderer, NULL);
+
+		//Drawing the hero
+		SDL_SetRenderDrawColor(renderer, hero.c->R, hero.c->G, hero.c->B, 0x00);
+		draw_h.x =hero.x;
+		draw_h.y =hero.y;
+		SDL_RenderFillRect(renderer, &draw_h);
+
+		SDL_RenderPresent(renderer);
+
+		if(get_out){
+			SDL_DestroyRenderer(renderer);
+			SDL_DestroyWindow(window);
+			SDL_Quit();
+			exit(0);
 		}
 	}
+	//FINALIZATION
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
